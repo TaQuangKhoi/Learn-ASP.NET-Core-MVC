a2b488bd0560125f3b83a85b4724b3fb6accc3e1
TaQuangKhoi <khoi0941@gmail.com>
2023-02-28 16:13:06 +0700

    Xong CreateViewModel

---
+++ ProductManagement/Models/CreateViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManager.Models;
+
+public class CreateViewModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Price { get; set; }
+    public IFormFile Photo { get; set; }
+}
