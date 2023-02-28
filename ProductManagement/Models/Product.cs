a8449964178375933eac891b5b1082d39d41148d
TaQuangKhoi <khoi0941@gmail.com>
2023-02-28 16:08:50 +0700

    Xong Product

---
+++ ProductManagement/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManager.Models;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Price { get; set; }
+    public string PhotoPath { get; set; }
+}
