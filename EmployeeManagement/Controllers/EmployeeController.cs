a5215c01bed02e97d7800f078526ce53c5176c7e
TaQuangKhoi <khoi0941@gmail.com>
2023-02-28 15:52:33 +0700

    Lưu file thành công

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyManagement.Models.Employees;
+using CompanyManagement.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyManagement.Controllers
@@ -6,13 +7,20 @@ namespace CompanyManagement.Controllers
     public class EmployeeController : Controller
     {
         private IEmployeeRepository _employeeRepository;
+        private IWebHostEnvironment _webHost;
         
         private readonly ILogger _logger;
+
         
-        public EmployeeController(IEmployeeRepository employeeRepository, ILogger<EmployeeController> logger)
+        public EmployeeController(
+            IEmployeeRepository employeeRepository, 
+            ILogger<EmployeeController> logger,
+            IWebHostEnvironment webHost
+            )
         {
             _logger = logger;
             _employeeRepository = employeeRepository;
+            _webHost = webHost;
         }
 
         public IActionResult Details(int ID)
@@ -31,16 +39,51 @@ namespace CompanyManagement.Controllers
             return View(employees);
         }
 
-        [HttpGet]
+        [HttpGet] // Khi load lên thì chỉ hiện nơi nhập data
         public IActionResult AddEmployee()
         {
+            string path = _webHost.ContentRootPath;
+            // _logger.LogInformation("ContentRootPath" + path);
             return View();
         }
         
-        [HttpPost]
-        public IActionResult AddEmployee(Employee employee)
+        [HttpPost] // Khi submit thì sẽ thực hiện hành động
+        public IActionResult AddEmployee(CreateViewModel model)
         {
+            _logger.LogInformation("Add Employee");
+            string fileName = "";
+            string path = _webHost.WebRootPath + "\\images\\";
+            
+            
+            if (model.Photo is null)
+            {
+                _logger.LogInformation("Photo is null");
+                fileName = "null";
+            }
+            else
+            {
+                fileName = model.Photo.FileName;
+            }
+            
+            model.Photo.CopyTo(new FileStream(path + fileName, FileMode.Create));
+            
+            _logger.LogInformation("fileName" + fileName);
+            _logger.LogInformation("WebRootPath" + path);
+
+            Employee employee = new Employee()
+            {
+                Name = model.Name,
+                Department = model.Department,
+                PhotoPath = fileName
+            };
+            
+            // B1: Thêm 1 nhân viên mới
             _employeeRepository.Add(employee);
+            
+            //B2: Upload hình của nhân viên lên server
+            // Trong thư mục wwwroot, thêm thư mục images
+            
+            
             return View("List", _employeeRepository.GetAllEmployees());
         }
 
--- EmployeeManagement/Models/ViewModels/CreateViewModel.cs
+++ EmployeeManagement/Models/ViewModels/CreateViewModel.cs
@@ -4,5 +4,5 @@ public class CreateViewModel
 {
     public string Name { get; set; }
     public string Department { get; set; }
-    public IFormFile Photo { get; set; }
+    public IFormFile? Photo { get; set; }
 }
