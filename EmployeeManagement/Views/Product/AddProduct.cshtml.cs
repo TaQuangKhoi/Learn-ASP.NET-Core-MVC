51721b88759f4896ac508e9b556b6320fa91bd76
TaQuangKhoi <khoi0941@gmail.com>
2023-02-28 16:24:42 +0700

    Xong Add View Product

---
+++ EmployeeManagement/Views/Product/AddProduct.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CompanyManagementNET7.Views.Product;
+
+public class AddProduct : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
