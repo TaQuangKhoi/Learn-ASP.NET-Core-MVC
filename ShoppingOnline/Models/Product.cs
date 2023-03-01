e9749e9183e1c0479b7dc1bdfa89d9d3d428fe2c
TaQuangKhoi <khoi0941@gmail.com>
2023-03-01 14:21:11 +0700

    Tạo model

---
+++ ShoppingOnline/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingOnline.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a product name")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a short description")]
+    public string ShortDescription { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a long description")]
+    public string LongDescription { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a category")]
+    public string Category { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a price")]
+    public decimal Price { get; set; }
+    
+    [Required(ErrorMessage = "Please enter a stock")]
+    public string Stock { get; set; }
+    
+}
