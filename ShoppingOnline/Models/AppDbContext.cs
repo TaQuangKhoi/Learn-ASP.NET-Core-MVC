21b90173c69cac7051f948429583a5cb2265038b
TaQuangKhoi <khoi0941@gmail.com>
2023-03-01 14:27:43 +0700

    Thêm connection string Sqlite

---
+++ ShoppingOnline/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ShoppingOnline.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    
+    public DbSet<Product> Products { get; set; }
+    
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            
+            optionsBuilder.UseSqlite(
+                "Data Source=c:\\mydb.db;Version=3;"
+            );
+            
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
