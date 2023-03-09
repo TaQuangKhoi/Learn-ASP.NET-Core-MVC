e14bdf4b9c76c6980946f3265870878641d378d5
TaQuangKhoi <khoi0941@gmail.com>
2023-03-09 20:45:02 +0700

    Thêm component NavigationMenuViewComponent

---
+++ ShoppingOnline/Controllers/ShoppingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingOnline.Models;
+using ShoppingOnline.Models.ViewModals;
+
+namespace ShoppingOnline.Controllers;
+
+public class ShoppingController : Controller
+{
+    IProductInterface productRepository;
+    
+    public ShoppingController(IProductInterface _productRepository)
+    {
+        productRepository = _productRepository;
+    }
+    
+    // GET
+    public IActionResult Index()
+    {
+        return View("ProductList");
+    }
+    
+    public IActionResult List(string category)
+    {
+        ProductListViewModel productListViewModel = new ProductListViewModel()
+        {
+            Products = productRepository.GetAllProducts().Where(p => category == null || p.Category == category).ToList(),
+            CurrentCategory = category
+        };
+        return View("ProductList", productListViewModel);
+    }
+}
---
+++ ShoppingOnline/Models/ViewModals/ProductListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingOnline.Models.ViewModals;
+
+public class ProductListViewModel
+{
+    public IEnumerable<Product> Products { get; set;}
+    
+    public string CurrentCategory { get; set;}
+}
