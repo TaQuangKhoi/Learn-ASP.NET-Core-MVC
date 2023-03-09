6a4642b282c48188015f385eb99a943352789aaf
TaQuangKhoi <khoi0941@gmail.com>
2023-03-09 11:02:44 +0700

    Copy code từ Sơn

---
+++ ShoppingOnline/Models/Cart.cs
@@ -0,0 +1,40 @@
+﻿namespace ShoppingOnline.Models;
+
+public class Cart
+{
+    private List<CartItem> ItemCollection = new List<CartItem>();
+    public virtual IEnumerable<CartItem> Items => ItemCollection;
+
+    public virtual void AddItem(Product product, int quanity)
+    {
+        CartItem Item = ItemCollection.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+        if (Item == null)
+        {
+            ItemCollection.Add(new CartItem
+            {
+                Product = product,
+                Quantity = quanity
+            });
+        }
+        else
+        {
+            Item.Quantity += quanity;
+        }
+    }
+
+    public virtual void RemoveItem(Product product) =>
+        ItemCollection.RemoveAll(l => l.Product.Id == product.Id);
+
+    public virtual decimal ComputeTotalValue() => ItemCollection.Sum(l => l.Product.Price * l.Quantity);
+
+    public virtual void Clear() => ItemCollection.Clear();
+}
+
+public class CartItem
+{
+    public int CartItemId { get; set; }
+
+    public Product Product { get; set; }
+
+    public int? Quantity { get; set; }
+}
