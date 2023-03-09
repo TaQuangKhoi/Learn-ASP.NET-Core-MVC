ab8086e375a94b848127b3ea2d7aff3c6fb439d7
Tạ Quang Khôi <khoi0941@gmail.com>
2023-03-09 11:48:34 +0700

    Tạo Cart Controller và View Index của nó
    Signed-off-by: TaQuangKhoi <khoi0941@gmail.com>

---
+++ ShoppingOnline/Controllers/CartController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingOnline.Infrastructure;
+using ShoppingOnline.Models;
+using ShoppingOnline.Models.ViewModals;
+
+namespace ShoppingOnline.Controllers;
+
+public class CartController : Controller
+{
+    private IProductInterface _productRepository;
+
+    public CartController(IProductInterface productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    
+    // GET
+    public ViewResult Index(string returnUrl)
+    {
+        return View(new CartIndexViewModel
+        {
+            Cart = GetCart(),
+            ReturnUrl = returnUrl
+        });
+    }
+
+    public RedirectToActionResult AddToCart(int id, string returnUrl)
+    {
+        Product product = _productRepository.GetAllProducts().FirstOrDefault(p => p.Id == id);
+        if (product != null)
+        {
+            Cart cart = GetCart();
+            cart.AddItem(product, 1);
+            SaveCart(cart);
+        }
+
+        return RedirectToAction("Index", new { returnUrl });
+    }
+
+    public RedirectToActionResult RemoveFromCart(int id, string returnUrl)
+    {
+        Product product = _productRepository.GetAllProducts().FirstOrDefault(p => p.Id == id);
+        if (product != null)
+        {
+            Cart cart = GetCart();
+            cart.RemoveItem(product);
+            SaveCart(cart);
+        }
+
+        return RedirectToAction("Index", new { returnUrl });
+    }
+
+    public RedirectToActionResult Clear(string returnUrl)
+    {
+        Cart cart = GetCart();
+        cart.Clear();
+        SaveCart(cart);
+        return RedirectToAction("Index", new { returnUrl });
+    }
+
+
+    private Cart GetCart()
+    {
+        Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+        return cart;
+    }
+
+    private void SaveCart(Cart cart)
+    {
+        HttpContext.Session.SetJson("Cart", cart);
+    }
+}
